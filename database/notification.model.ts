2e679e1f9052ac817936e98b09293eb52ab7a2d4
wahuu2 <lilywahu1@gmail.com>
2026-09-01 15:48:44 +0300

    Create the Notification model

---
+++ database/notification.model.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const notificationSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: [
+        "booking_confirmed",
+        "payment_successful",
+        "ticket_generated",
+        "event_updated",
+        "event_cancelled",
+        "event_reminder",
+      ],
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+    },
+
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Notification =
+  models.Notification ||
+  mongoose.model("Notification", notificationSchema);
+
+export default Notification;
